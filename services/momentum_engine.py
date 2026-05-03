7e2c09d216d929be417510c6e86336605101e86d
ankur <ankd99@gmail.com>
2026-05-03 18:59:15 +0530

    yahoo finance

--- services/momentum_engine.py
+++ services/momentum_engine.py
@@ -13,9 +13,9 @@ def calculate_momentum(df):
     df["Close"] = pd.to_numeric(df["Close"], errors="coerce")
 
     # RETURNS
-    df["return_12m"] = df["Close"] / df["Close"].shift(252) - 1
-    df["return_6m"]  = df["Close"] / df["Close"].shift(126) - 1
-    df["return_3m"]  = df["Close"] / df["Close"].shift(63) - 1
+    df["return_12m"] = df["Close"] / df["Close"].shift(200) - 1
+    df["return_6m"]  = df["Close"] / df["Close"].shift(100) - 1
+    df["return_3m"]  = df["Close"] / df["Close"].shift(50) - 1
 
     df = df.dropna(subset=["return_12m", "return_6m", "return_3m"])
 
