a847431017c14a65cbcaeb766c512acdd6055ed7
ankur <ankd99@gmail.com>
2026-06-01 22:08:41 +0530

    value wrapper of integers

---
+++ llm-mini/back-propagation/value_function_custom_object.py
@@ -0,0 +1,20 @@
+class Value:
+    def __init__(self,data, _children=(), _op='', label=''):
+        self.data = data
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+    def __repr__(self):
+        return f"Value(data={self.data})"
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), '+')
+        return out
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '*')
+        return out
+
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10, label='c')
+d = a*b + c
+d
