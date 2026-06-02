12382e40ff8b4096d1d3b17da27c1e79b2579d52
ankur <ankd99@gmail.com>
2026-06-02 22:13:28 +0530

    creating tanh function in value class for neuron

--- llm-mini/llm-mini/back-propagation/neuron.py
+++ llm-mini/llm-mini/back-propagation/neuron.py
@@ -6,7 +6,7 @@ w1 = Value(-3.0, label='w1')
 w2 = Value(1.0, label='w2')
 
 #bias of NN
-b = Value(6.7, label='b')
+b = Value(6.8813735870195432, label='b')
 
 x1w1 =  x1 * w1
 x1w1.label = 'x1w1'
@@ -20,12 +20,12 @@ x1w1x2w2.label = 'x1w1x2w2'
 n = x1w1x2w2 + b
 n.label = 'n'
 
-o = n.tanh()
-draw_dot(o)
+o = n.tanh(); o.label = 'o'
 
-#gradients filling
-do/do = 1
-o = tanh(n)
-do/dn = 1 - tanh(n)**2
-do/dn = 1 - o.data**2 ##= 0.5
-n.grad = 0.5
+o.grad = 1.0
+#o = tanh(n)**2
+#do/dn = 1 - tanh(n)**2
+#do/dn = 1 - o.data**2
+n.grad = 1 - o.data**2
+
+draw_dot(o)
