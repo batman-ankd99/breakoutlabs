e8512419f825e70c9a85cc456d6664a48606feeb
ankur <ankd99@gmail.com>
2026-06-03 22:40:53 +0530

    backward pass for tanh function

--- llm-mini/llm-mini/back-propagation/value_function_custom_object.py
+++ llm-mini/llm-mini/back-propagation/value_function_custom_object.py
@@ -5,6 +5,8 @@ class Value:
         self._op = _op
         self.label = label
         self.grad = 0.0
+        self._backward = lambda: None
+
     def __repr__(self):
         return f"Value(data={self.data})"
     def __add__(self, other):
@@ -17,7 +19,11 @@ class Value:
     def tanh(self):
         x = self.data
         t = (math.exp(2*x)-1)/(math.exp(2*x)+1)
-        out = Value(t, (self,), 'tanh')
+        out = Value(t, (self,), 'tanh')      # ye aage waali node O hogyi maan lete hai
+
+        def _backward():
+            self.grad += (1 - t**2) * out.grad       # iska formula banta  - 1 - o**2, ab is case me to o but koi bhi ho sakti hai, upar out aage wali node hi hai, out.grad coming from ahead we multiple ahead gradient with local gradient. out.grad is ahead nodes gradient which is 1 here.
+            out._backward = _backward()
         return out
 
 a = Value(2.0, label='a')
