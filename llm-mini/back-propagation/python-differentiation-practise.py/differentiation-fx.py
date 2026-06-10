558b36848340245efde313f2e49e5fd444ccf0d4
ankur <ankd99@gmail.com>
2026-06-10 16:02:20 +0530

    differentiation with loop finding gradient to minimise loss

--- llm-mini/back-propagation/python-differentiation-practise.py/differentiation-fx.py
+++ llm-mini/back-propagation/python-differentiation-practise.py/differentiation-fx.py
@@ -12,3 +12,21 @@ for i in range(170):
 
 print(x)
 print(slope)
+
+a, b, c = 2.0, -3.0, 10.0
+h = 0.0001
+lr = 0.01
+
+def f(a, b, c):
+    return a*b + c   # this is the expression Karpathy uses
+
+for i in range(100):
+    # numerical gradient for each input
+    da = (f(a+h, b, c) - f(a, b, c)) / h
+    db = (f(a, b+h, c) - f(a, b, c)) / h
+    dc = (f(a, b, c+h) - f(a, b, c)) / h
+
+    # update each input
+    a = a - lr * da
+    b = b - lr * db
+    c = c - lr * dc
