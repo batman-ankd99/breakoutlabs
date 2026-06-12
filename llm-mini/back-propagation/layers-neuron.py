356c8c2842d07cbbdc0626a2e8556b9794a902bc
ankur <ankd99@gmail.com>
2026-06-12 21:56:17 +0530

    neuron and layer

--- llm-mini/back-propagation/layers-neuron.py
+++ llm-mini/back-propagation/layers-neuron.py
@@ -1,3 +1,11 @@
 class Layers:
   def __init__(self, nin, nout):
-    
+    self.neurons = []
+    for i in range(nout):
+      self.neurons.append(Neuron(nin))
+
+  def __call__(self, x):
+    outs = []
+    for neuro in self.neurons:
+      out.append(neuro(x))
+    return outs
