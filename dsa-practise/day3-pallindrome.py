4ac70ce7be8fa15812e6d969a7849aa5f30c5f75
ankur <ankd99@gmail.com>
2026-07-09 00:18:23 +0530

    day 3 pallindrome - method 2

--- dsa-practise/day3-pallindrome.py
+++ dsa-practise/day3-pallindrome.py
@@ -2,6 +2,17 @@ Day 3 (07-07-2026) — Problem: Check if a List is a Palindrome
 Statement:
 Write a Python function that takes a list (of integers) and returns True if it reads the same forwards and backwards, and False otherwise — without using slicing (list[::-1]) or the built-in reversed().
 
+def pallin_2(list_num):
+    rev_num = []
+    for i in range(len(list_num)-1,-1,-1):
+      rev_num.append(list_num[i])
+
+    if list_num == rev_num:
+      print("list is pallindrome")
+    else:
+      print("it is not")
+
+
 
 def pallin_1(list_num):
     is_pallin = True
@@ -21,3 +32,4 @@ for i in range(n):
   list_num.append(elem)
 
 pallin_1(list1)
+pallin_2(list1)
