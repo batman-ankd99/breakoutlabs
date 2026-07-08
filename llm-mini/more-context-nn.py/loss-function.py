88318a414ad60f76a2dbcc097bc009ab6485efb2
ankur <ankd99@gmail.com>
2026-07-08 16:28:31 +0530

    1000 runs on random 32 saopmple

--- llm-mini/more-context-nn.py/loss-function.py
+++ llm-mini/more-context-nn.py/loss-function.py
@@ -1,32 +1,35 @@
-def lossfunc(Xbatch, Ybatch):
-  for _ in range(50):
-    ## forward pass
-    Xenc = C[Xbatch]
-    Xenc = torch.cat(torch.unbind(Xenc,dim=1),dim=1)
-    import torch.nn.functional as F
-    h = torch.tanh((Xenc @ W1) + b1)
-    logits = (h @ W2) + b2
-    #counts = logits.exp()
-    #probs = counts/counts.sum(dim=1, keepdim=True)
-    loss = F.cross_entropy(logits, Ybatch)
+import torch.nn.functional as F
+#def lossfunc(Xbatch, Ybatch):
+for _ in range(1000):
+  ix = torch.randint(0, len(X), (32,)) #sampling any random 32 indexes from 2 lakh indexes
 
-    #grads to None
-    for params in parameters:
-      params.grad = None
+  ## forward pass
+  emb = C[X[ix]]    #(32,3,2)
+#    h = torch.cat(torch.unbind(Xenc,dim=1),dim=1)
+  h = torch.tanh(emb.view(-1,6) @ W1 + b1)
+  logits = (h @ W2) + b2
+  #counts = logits.exp()
+  #probs = counts/counts.sum(dim=1, keepdim=True)
+  loss = F.cross_entropy(logits, Y[ix])
 
-    #backward
-    loss.backward()
-    #update
-    lr = 0.01
-    for params in parameters:
-      params.data =  params.data + (-params.grad * lr)
+  #backward
+  #grads to None
+  for p in parameters:
+    p.grad = None
+  loss.backward()
+  #update
+  lr = 0.01
+  for p in parameters:
+    p.data =  p.data + (-p.grad * lr)
 
-  print(loss.item())
+print(loss.item())
 
 
-## sampling 32 data samples
 
-ix = torch.randint(0, len(X), (32,))     #sampling any random 32 indexes from 2 lakh indexes of total sample
-Xdata = X[ix]    #not embeded yet
-Ydata = Y[ix]
-lossfunc(Xdata, Ydata)
+#2nd function - to get Loss overall complete data
+
+emb = C[X]
+h = torch.tanh(emb.view(-1,6) @ W1 + b1)
+logits = h @ W2 + b2
+loss = F.cross_entropy(logits, Y)
+loss
