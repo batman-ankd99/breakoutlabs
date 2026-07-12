f69b952dbec6cefe8c09d1c4ba27e21e06fef607
ankur <ankd99@gmail.com>
2026-07-12 11:49:27 +0530

    day5 - unique elements dict

--- dsa-practise/day5-countelem.py
+++ dsa-practise/day5-countelem.py
@@ -4,3 +4,22 @@ Write a Python function that takes a list of integers and returns a dictionary s
 Example:
 pythonInput: [4, 2, 4, 5, 2, 4, 7, 1]
 Output: {4: 3, 2: 2, 5: 1, 7: 1, 1: 1}
+
+def count_elem(list_num):
+    dict_elem = {}
+    for elem in list_num:
+        if elem in dict_elem:
+            dict_elem[elem] += 1
+        else:
+            dict_elem[elem] = 1
+    return dict_elem
+
+list1 = []
+n = int(input("enter length of list"))
+for i in range(n):
+  elem = int(input("enter numbers in a list : "))
+  list1.append(elem)
+
+
+result = count_elem(list1)
+print(result)
