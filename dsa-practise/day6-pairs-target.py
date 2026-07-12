4a8a25779834bee2b6c397bfcc7d446bdce6cd0c
ankur <ankd99@gmail.com>
2026-07-12 11:50:59 +0530

    day6 - sum of pair - target

---
+++ dsa-practise/day6-pairs-target.py
@@ -0,0 +1,6 @@
+Day 6 (10-07-2026) — Problem: Find All Pairs That Sum to a Target
+Statement:
+Write a Python function that takes a list of integers and a target sum, and returns all unique pairs of numbers from the list that add up to the target — without using nested loops (no for i inside for j).
+Example:
+pythonInput: list = [2, 7, 4, 1, 5, 3], target = 6
+Output: [(2, 4), (1, 5)]
