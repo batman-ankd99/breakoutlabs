839dcc2d29c10455c5237be06278213133682e2d
ankur <ankd99@gmail.com>
2026-07-13 13:50:41 +0530

    day 6 - target sum

---
+++ dsa-practise/day7-rotatelist.py
@@ -0,0 +1,5 @@
+Day 7 (11-07-2026) — Problem: Rotate a List by K Positions
+Write a Python function that takes a list of integers and an integer k, and returns the list rotated to the right by k positions — without using slicing.
+Example:
+Input: list = [1, 2, 3, 4, 5], k = 2
+Output: [4, 5, 1, 2, 3]
