aedc377406891f382b3dae3f48c86615ab8c7596
ankur <ankd99@gmail.com>
2026-07-13 13:10:03 +0530

    day 6 - target sum

--- dsa-practise/day6-pairs-target.py
+++ dsa-practise/day6-pairs-target.py
@@ -4,3 +4,18 @@ Write a Python function that takes a list of integers and a target sum, and retu
 Example:
 pythonInput: list = [2, 7, 4, 1, 5, 3], target = 6
 Output: [(2, 4), (1, 5)]
+
+
+def target_sum(list_num, target):
+    pairs = []
+    for i in range(len(list_num)-1):
+        if list_num[i] + list_num[i+1] == 10:
+            pairs.append((i,j))
+
+list1 = []
+n = int(input("enter length of list"))
+for i in range(n):
+  elem = int(input("enter numbers in a list : "))
+  list1.append(elem)
+
+target_sum(list1, 10)
