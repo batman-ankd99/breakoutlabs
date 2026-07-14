eecd0097bcb13f07dd6b72f06cc105991e5d6557
ankur <ankd99@gmail.com>
2026-07-14 22:01:11 +0530

    day 7 - rotate right list

--- dsa-practise/day7-rotatelist.py
+++ dsa-practise/day7-rotatelist.py
@@ -3,3 +3,22 @@ Write a Python function that takes a list of integers and an integer k, and retu
 Example:
 Input: list = [1, 2, 3, 4, 5], k = 2
 Output: [4, 5, 1, 2, 3]
+
+def rotate_list(list_num, k):
+    rotated_list = []
+    n = len(list_num)
+    for i in range(n-k,n):
+        rotated_list.append(list_num[i])
+    print("original list ", list_num)
+    for i in range(k+1):
+        rotated_list.append(list_num[i])
+    return rotated_list
+
+list1 = []
+n = int(input("enter length of list"))
+for i in range(n):
+  elem = int(input("enter numbers in a list : "))
+  list1.append(elem)
+
+result = rotate_list(list1, 2)
+print(result)
